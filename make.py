b1688cb53fbc9c5c806036c971d816240c9b335f
ilyaleshchik <lurkudismal88@gmail.com>
2022-01-20 22:57:18 +0300

    CLEAN

--- make.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-
-os.system("g++ -c main.cpp")
-os.system("g++ -c myobj.cpp")
-os.system("g++ -c sorting.cpp")
-os.system("g++ main.o myobj.o sorting.o -o app -lsfml-graphics -lsfml-window -lsfml-system")
